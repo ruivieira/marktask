223814ea85fdabb7a69f7e70733cc008270f3951
Rui Vieira <ruidevieira@googlemail.com>
2024-02-05 22:13:08 +0000

    Update version to 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub mod dates;
 
 fn main() {
     let matches = App::new("marktask")
-        .version("0.1.0")
+        .version("0.2.0")
         .about("Processes Markdown tasks")
         .arg(Arg::with_name("json")
             .long("json")
